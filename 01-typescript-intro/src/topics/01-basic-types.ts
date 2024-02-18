11ebce7f2088a51f60c329347466167454695420
Andres Godoy <andres_amilcargg@hotmail.com>
2024-02-18 14:49:34 -0700

    Bases typescript

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+
+const name: string = 'Strider';
+
+let hpPoints: number | 'FULL' = 195; 
+
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+
+
+export {};
