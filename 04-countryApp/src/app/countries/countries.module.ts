4d8af4601d38f5853864821016dd95131593546f
ANDRES AMILCAR GONZALEZ GODOY <andres.gonzalez@coppel.com>
2024-03-02 10:29:26 -0700

    avance countryApp

--- 04-countryApp/src/app/countries/countries.module.ts
+++ 04-countryApp/src/app/countries/countries.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { CountriesRoutingModule } from './countries-routing.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
-import { CountriesRoutingModule } from './countries-routing.module';
-import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -14,7 +16,8 @@ import { SharedModule } from '../shared/shared.module';
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { CountriesService } from '../../services/countries.service';
+
+import { Country } from '../../interfaces/country';
+
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
@@ -7,9 +11,14 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService ) {}
+
   searchByCapital( term: string ): void {
-    console.log('Desde ByCapitalPage')
-    console.log({term})
+    this.countriesService.searchCapital( term ).subscribe( countries => {
+      this.countries = countries;
+    });
   }
 
 }
